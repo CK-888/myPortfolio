f75483aeebc0a19afedfff13180dc220557158cb
ck0629 <kuruta.dev00@gmail.com>
2020-03-15 14:47:15 +0900

    test

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ test }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      default: "test",
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
